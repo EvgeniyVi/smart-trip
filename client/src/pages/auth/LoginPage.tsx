ece7a1346daa387ecd5e31ce629e586e399fb5ee
evhenii <e.vinnikov@flex-solution.com>
2023-09-21 12:01:39 +0300

    00004 - [smart_trip] - Created auth

---
+++ client/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>hello</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default LoginPage
