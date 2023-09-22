b038b608881f8c704d48ae4f914498436414be01
evhenii <e.vinnikov@flex-solution.com>
2023-09-22 21:13:45 +0300

    00005 - [smart_trip] - Created auth layout

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react'
+import './style.scss'
+import { Form, Input } from 'antd'
 
 function LoginPage() {
+  const [form] = Form.useForm()
   return (
-    <div>
-      <div>
-        <div>
-          <div>hello</div>
-        </div>
+    <div className="wrapper_auth_page">
+      <div className="container_auth">
+        <Form form={form}>
+          <Input />
+        </Form>
       </div>
     </div>
   )
 }
+
 export default LoginPage
